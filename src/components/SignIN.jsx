59c8239efbcc0bf885b0eeeffbc09765b3b13e7a
Abytt <abytt5823@gmail.com>
2024-06-13 17:52:58 +0530

    test10

--- src/components/SignIN.jsx
+++ src/components/SignIN.jsx
@@ -1,12 +1,13 @@
 import axios from 'axios'
 import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
 
 
-const SignIN = () => {
+const SignUp = () => {
 
     const [data, setdata] = useState([
         {
-           
+           "name":{type:String,required:true},
         "Email":{type:String,required:true},
         "password":{type:String,required:true}
 
@@ -17,7 +18,7 @@ const SignIN = () => {
     }
     const readvalue = () => {
         console.log(data)
-        axios.post("http://localhost:8080/SignIN", data).then(
+        axios.post("http://localhost:8080/SignUp", data).then(
             (response) => {
                 if (response.data.Status=="Success") {
                     alert("Data added Successfully")
@@ -27,7 +28,6 @@ const SignIN = () => {
             }
         ).catch().finally()
     }
-    
     return (
         <div>
 
@@ -36,7 +36,10 @@ const SignIN = () => {
                 <div className="row">
                     <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
                         <div className="row g-3">
-                        
+                        <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                                <label htmlFor="" className="label form-label">name</label>
+                                <input type="text" className="input form-control" name='name' value={data.name} onChange={InputHandler}/>
+                            </div>
                             <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
                                 <label htmlFor="" className="label form-label">Email</label>
                                 <input type="text" className="input form-control" name='Email' value={data.Email} onChange={InputHandler}/>
@@ -48,8 +51,8 @@ const SignIN = () => {
                             </div>
                            
                             <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
-                                <button className="btn btn-warning" onClick={readvalue}>SignIN</button>
-                                <button className="btn btn-warning" onClick={readvalue}>SignIN</button>
+                                <button className="btn btn-warning" onClick={readvalue}>SignUP</button>
+                                <Link to="/SignIN"></Link>
                             </div>
 
                         </div>
@@ -60,4 +63,4 @@ const SignIN = () => {
     )
 }
 
-export default SignIN
+export default SignUp
